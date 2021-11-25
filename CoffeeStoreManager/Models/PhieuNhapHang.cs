491347fc658952259d12f06915302e8b3eae01c1
khanh <JBkhanhtran@gmail.com>
2021-11-25 09:13:56 +0700

    chinh sua phan employee va statistic

--- CoffeeStoreManager/Models/PhieuNhapHang.cs
+++ CoffeeStoreManager/Models/PhieuNhapHang.cs
@@ -14,18 +14,15 @@ namespace CoffeeStoreManager.Models
     
     public partial class PhieuNhapHang
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public PhieuNhapHang()
         {
             this.CT_PhieuNhapHang = new HashSet<CT_PhieuNhapHang>();
         }
-    
+        public virtual ICollection<CT_PhieuNhapHang> CT_PhieuNhapHang { get; set; }
         public int ma_phieu_nhap_hang { get; set; }
         public Nullable<System.DateTime> ngay_nhap { get; set; }
         public string nha_cung_cap { get; set; }
         public Nullable<decimal> tong_tien { get; set; }
-    
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<CT_PhieuNhapHang> CT_PhieuNhapHang { get; set; }
+
     }
 }
