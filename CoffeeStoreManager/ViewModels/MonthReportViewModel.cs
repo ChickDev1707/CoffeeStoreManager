8eb668e8ddc84cd7fd2e83aba145fcee1e2c13a0
ChickDev1707 <thuancoixy234786@gmail.com>
2021-12-23 20:28:42 +0700

    change title

--- CoffeeStoreManager/ViewModels/MonthReportViewModel.cs
+++ CoffeeStoreManager/ViewModels/MonthReportViewModel.cs
@@ -8,25 +8,33 @@ namespace CoffeeStoreManager.ViewModels
 {
     public class MonthReportViewModel:BaseViewModel
     {
-        private ObservableCollection<MonthReport> report;
         public ObservableCollection<MonthReport> Report { get => report; set { report = value; OnPropertyChanged(nameof(report)); } }
+        private ObservableCollection<MonthReport> report;
 
-        private DateTime targetTime;
         public ICommand RenderReport { get; set; }
-        public DateTime TargetTime { get => targetTime; set { targetTime = value; OnPropertyChanged(nameof(targetTime)); } }
-
+        public int Month { get => month; set { month = value; OnPropertyChanged(nameof(Month)); } }
+        private int month;
+        public int Year { get => year; set { year = value; OnPropertyChanged(nameof(Year)); } }
+        private int year;
         public MonthReportViewModel()
         {
             RenderReport = new RelayCommand<object>((p) => { return true; }, renderReport);
+            loadTime();
         }
-
+        private void loadTime()
+        {
+            var now = DateTime.Now;
+            Month = now.Month;
+            Year = now.Year;
+        }
         private void renderReport(object obj)
         {
             var db = DataProvider.Ins.DB;
-            var sqlString = "select ten_loai_mon_an, sum(so_luong) as tong_so_luong, sum(thanh_tien) as tong_so_tien "
+            var sqlStringFormat = "select ten_loai_mon_an, sum(so_luong) as tong_so_luong, sum(thanh_tien) as tong_so_tien "
                             + "from LoaiMonAn as fType, MonAn as f, HoaDon as bill, CT_HoaDon as dBill "
-                            + "where fType.ma_loai_mon_an = f.ma_loai_mon_an and f.ma_mon_an = dBill.ma_mon_an and dBill.ma_hoa_don = bill.ma_hoa_don and month(ngay_xuat_hoa_don) = 11 and year(ngay_xuat_hoa_don) = 2021 "
+                            + "where fType.ma_loai_mon_an = f.ma_loai_mon_an and f.ma_mon_an = dBill.ma_mon_an and dBill.ma_hoa_don = bill.ma_hoa_don and month(ngay_xuat_hoa_don) = {0} and year(ngay_xuat_hoa_don) = {1} "
                             + "group by fType.ma_loai_mon_an, ten_loai_mon_an";
+            var sqlString = String.Format(sqlStringFormat, Month, Year);
             var dataList = db.Database.SqlQuery<MonthReport>(sqlString).ToList();
             Report = new ObservableCollection<MonthReport>(dataList);
         }
